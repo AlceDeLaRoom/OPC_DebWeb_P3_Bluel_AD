72204dc692b54221e9b2a308a1ecdfff18477acd
AlceDeLaRoom <alicedebrans@gmail.com>
2024-03-04 15:14:54 +0100

    Delete works (need to fix the '.catch') + BAD IDEA add user/auth for verify the token in the API/Back-end

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/users.controller');
+const auth = require('../middlewares/auth');
 
 router.post('/login', userCtrl.login);
+router.get('/auth', auth);
 //router.post('/signup', userCtrl.signup);
 
 module.exports = router;
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,3 +1,5 @@
+const TOKEN = JSON.parse(localStorage.getItem("token")).token;
+console.log(TOKEN);
 /* get/post/delete api */
 async function apiGet() {
     const response = await fetch("http://localhost:5678/api/works", {});
@@ -11,6 +13,17 @@ async function apiGetCategory(){
     let data = await response.json();
     // console.log ("données récupérées: ", data)
     return data;
+}
+
+async function apiDelete(id){
+    console.log("delete");
+    await fetch('http://localhost:5678/api/works/'+id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer " + TOKEN
+        }
+    }).catch((error)=>{console.error(error)})
 }
 
 /* add categories's buttons */
@@ -30,7 +43,7 @@ function addBtn(categorie, selectedBtn = ""){
     const txtBtn = document.createElement("p");
     txtBtn.innerHTML = categorie;
     btnCat.appendChild(txtBtn);
-    btnCat.addEventListener("click", function(){/selected/.test(btnCat.className)? console.log("Already selected!"):clicCat(btnCat)});
+    btnCat.addEventListener("click", ()=>{/selected/.test(btnCat.className)? console.log("Already selected!"):clicCat(btnCat)});
     return btnCat;
 };
 
@@ -45,14 +58,14 @@ function clicCat(btn){
 /* show work, category = "Tous" for all, don't forget to clean up gallery */
 function showWorks(category = "Tous" , contenor = gallery){
     if (/Tous/.test(category)) {
-        console.log("on affiche tout!")
+        /*console.log("on affiche tout!")*/
         for (const cat of categories){
             showWorks(cat.name, contenor);
         }
     }
     else{
         category = category.replace(/&amp;/g,"&");
-        console.log("on affiche ", category, "dans ", contenor)
+        /*console.log("on affiche ", category, "dans ", contenor)*/
         const title = RegExp("delete").test(contenor.className)? false : true;
         for (const work of works){
             if (RegExp(category).test(work.category.name)){
@@ -63,7 +76,7 @@ function showWorks(category = "Tous" , contenor = gallery){
 }
 
 function showOnework(work, title = true){
-    console.log("Add work: ", work.title)
+    /*console.log("Add work: ", work.title)*/
     const newWork = document.createElement("div");
     newWork.className = "work";
     const img = document.createElement("img");
@@ -83,9 +96,28 @@ function showOnework(work, title = true){
         const icon = document.createElement("i");
         icon.className = "fa-solid fa-trash-can";
         btnIcon.appendChild(icon);
+        btnIcon.addEventListener("click", ()=>{deleteWork(work.id)});
     };
 
     return newWork;
+}
+
+async function deleteWork(id){
+    await apiDelete(id)
+    .then(refreshWorks())
+    .catch((error)=>{console.log(error)});
+}
+
+function refreshWorks(){
+    gallery.innerHTML = '';
+    deleteGallery.innerHTML = '';
+    loadWorks();
+    console.log("on viens de refresh!")
+};
+
+function loadWorks(){
+    showWorks();
+    showWorks("Tous", deleteGallery);
 }
 
 
@@ -97,10 +129,6 @@ gallery.parentNode.insertBefore(addButtonCategories(categories), gallery.previou
 
 let works = await apiGet();
 
-showWorks();
-showWorks("Tous", deleteGallery);
+loadWorks();
 
-
-
-console.log(localStorage.getItem("token"));
 // Supprimer le token du localStorage localStorage.removeItem("token");
